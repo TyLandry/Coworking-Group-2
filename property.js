698eeb757433cd7c819f2956b0155ab19cb9e4bc
Jansen Agcaoili <ajagcaoili4@gmail.com>
2025-08-18 16:15:04 -0600

    Changed the variabe name to worskpace form workplace
    changed something from property.js and workspace.js

--- property.js
+++ property.js
@@ -1,42 +1,39 @@
 // property.js
 import mongoose from "mongoose";
-
+//import Workspace from "./workspace.js";
 
 // for the workspace
-const workspaceSchema = new mongoose.Schema(
-  {
-    name: String,           
-    unit: String,          
-    sqft: Number,
-    seats: Number,
-    price: Number,          
-    availability: Boolean,
-    smoking: Boolean,
-    term: { type: String, enum: ["day", "week", "month"] },
-  },
-  { _id: true } // it is used to edi/ delete the subdocument
-);
+// const workspaceSchema = new mongoose.Schema(
+//   {
+//     name: String,           
+//     unit: String,          
+//     sqft: Number,
+//     seats: Number,
+//     price: Number,          
+//     availability: Boolean,
+//     smoking: Boolean,
+//     term: { type: String, enum: ["day", "week", "month"] },
+//   },
+//   { _id: true } // it is used to edi/ delete the subdocument
+// );
 
 const propertySchema = new mongoose.Schema(
   {
-    //the owner manages this part
-    address: String,
+    address: { type: String, required: true },
     neighborhood: String,
     sqft: Number,
     parking: Boolean,
     publicTransportation: Boolean,
-    seats: Number,          
+    seats: Number,
     smoking: Boolean,
-    availability: Boolean, 
+    availability: Boolean,
     term: { type: String, enum: ["day", "week", "month"] },
-    price: Number,         
-    ownerEmail: String,
-
-    
+    price: Number,
+    ownerEmail: { type: String, required: true },
     //workspaces then attached to the owner's info
-    workspaces: [workspaceSchema],
+    //workspaces: [Workspace],
   },
-  { timestamps: true }
+  { timestamps: true, toJSON: { virtuals: true }, toObject: { virtuals: true } }
 );
 
 export default mongoose.model("Property", propertySchema);
