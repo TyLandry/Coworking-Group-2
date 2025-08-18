028f865fc37ce4233ec34df026a6cc1af3e41844
Jansen Agcaoili <ajagcaoili4@gmail.com>
2025-08-17 23:02:20 -0600

    Debug login, added additional comments

--- routes.js
+++ routes.js
@@ -51,7 +51,8 @@ router.post("/login", async (req, res) => {
     const { email, password } = req.body;
     
     // Find the user by email
-    const user = await User.findOne({ email })//.select("+password");
+    //Reinforce the password when making an API request
+    const user = await User.findOne({ email }).select("+password");
     if (!user) return res.status(400).json({ message: "User not found" });
     
     // Compare the password with hashed password in database
@@ -82,7 +83,7 @@ router.post("/login", async (req, res) => {
 //---------- Profile route ----------
 //Method: GET/profile
 //URL: "http://localhost:3000/api/profile"
-//This requires a valid JWT token and it will be recieved from login 
+//This requires a valid JWT token and it will be received from login 
 
 router.get("/profile", authMiddleware, async (req, res) => {
   try {
--- user.js
+++ user.js
@@ -1,8 +1,14 @@
 import mongoose from "mongoose";
 import bcrypt from "bcryptjs";
 
+//Add a schema to make sure that everything will be in line
 const userSchema = new mongoose.Schema({
-  
+  firstName: { type: String, required: true },
+  lastName:  { type: String, required: true },
+  email:     { type: String, required: true, unique: true },
+  phone:     { type: String },
+  password:  { type: String, required: true, select: false },
+  role:      { type: String, default: "user" }
 });
 
 // Hash password before saving
