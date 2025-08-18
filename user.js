8f65bda6199adf9929957ad4a33a4fd358be06cc
Leiziane Trevisan Dardin <leiziane.trevisan@yahoo.com.br>
2025-08-18 10:27:04 -0600

    CRUD for Owner and Coworker Search started - Correction o User.js

--- user.js
+++ user.js
@@ -3,24 +3,12 @@ import bcrypt from "bcryptjs";
 
 //Add a schema to make sure that everything will be in line
 const userSchema = new mongoose.Schema({
-  firstName: {type: String, required: true},
-  lastName: { type: String, required: true},
-  email: {type: String, required: true, unique: true},
-  phone: {type: String},
-  password:  {type: String, required: true, select: false},
-  role: {type: String, default: "user"}
+  firstName: { type: String, required: true },
+  lastName:  { type: String, required: true },
+  email:     { type: String, required: true, unique: true },
+  phone:     { type: String },
+  password:  { type: String, required: true, select: false },
+  role:      { type: String, default: "user" }
 });
-
-// Hash password before saving
-userSchema.pre("save", async function(next) {
-  if (!this.isModified("password")) return next();
-  this.password = await bcrypt.hash(this.password, 10);
-  next();
-});
-
-// Compare password method
-userSchema.methods.comparePassword = async function(candidatePassword) {
-  return await bcrypt.compare(candidatePassword, this.password);
-};
 
 export default mongoose.model("User", userSchema);
