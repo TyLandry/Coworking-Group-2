312095c878e13cc6381bebaebfed9dc229e4ed39
Jansen Agcaoili <ajagcaoili4@gmail.com>
2025-08-17 23:06:47 -0600

    clean up code for users

--- user.js
+++ user.js
@@ -3,12 +3,12 @@ import bcrypt from "bcryptjs";
 
 //Add a schema to make sure that everything will be in line
 const userSchema = new mongoose.Schema({
-  firstName: { type: String, required: true },
-  lastName:  { type: String, required: true },
-  email:     { type: String, required: true, unique: true },
-  phone:     { type: String },
-  password:  { type: String, required: true, select: false },
-  role:      { type: String, default: "user" }
+  firstName: {type: String, required: true},
+  lastName: { type: String, required: true},
+  email: {type: String, required: true, unique: true},
+  phone: {type: String},
+  password:  {type: String, required: true, select: false},
+  role: {type: String, default: "user"}
 });
 
 // Hash password before saving
